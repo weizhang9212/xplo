dd14db0aa4993e0320bc81c28aac896479418f54
liyuan <liyuan@umail.ucsb.edu>
2018-07-13 09:32:20 -0700

    Update post style

--- src/components/MarkersHandler/MarkersHandler.js
+++ src/components/MarkersHandler/MarkersHandler.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MarkerClusterer from '@google/markerclusterer'
+import './MarkersHandler.css'
 
 const MarkersHandler = (props) =>{
     const dream = [
@@ -22,25 +23,130 @@ const MarkersHandler = (props) =>{
         
         cluster.actionMade();
     }
+    var Popup;
+    Popup = function (position, content) {
+            this.position = position;
+            if (!content){
+                content = document.createElement('div');
+                content.innerHTML = 'LoadContent Error!';
+            }
+            content.classList.add('popup-bubble-content');
+            var pixelOffset = document.createElement('div');
+            pixelOffset.classList.add('popup-bubble-anchor');
+            pixelOffset.appendChild(content);
 
+            this.anchor = document.createElement('div');
+            this.anchor.classList.add('popup-tip-anchor');
+            this.anchor.appendChild(pixelOffset);
+
+            // Optionally stop clicks, etc., from bubbling up to the map.
+            this.stopEventPropagation();
+            };
+          // NOTE: google.maps.OverlayView is only defined once the Maps API has
+          // loaded. That is why Popup is defined inside initMap().
+          Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+
+          /** Called when the popup is added to the map. */
+          Popup.prototype.onAdd = function() {
+            this.getPanes().floatPane.appendChild(this.anchor);
+          };
+
+          /** Called when the popup is removed from the map. */
+          Popup.prototype.onRemove = function() {
+            if (this.anchor.parentElement) {
+              this.anchor.parentElement.removeChild(this.anchor);
+            }
+          };
+          /** Called when the popup needs to draw itself. */
+          Popup.prototype.draw = function() {
+
+            var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+            //Hide the popup when it is far out of view.
+            var display =
+                Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+                'block' :
+                'none';
+
+            if (display === 'block') {
+              this.anchor.style.left = divPosition.x + 'px';
+              this.anchor.style.top = divPosition.y + 'px';
+            }
+            if (this.anchor.style.display !== display) {
+              this.anchor.style.display = display;
+            }
+          };
+          Popup.prototype.getPosition = function() {
+            return this.postion; 
+        };
+
+        Popup.prototype.hide = function() {
+          if (this.anchor) {
+            // The visibility property must be a string enclosed in quotes.
+            this.anchor.style.visibility = 'hidden';
+          }
+        };
+
+        Popup.prototype.show = function() {
+          if (this.anchor) {
+            this.anchor.style.visibility = 'visible';
+          }
+        };
+          /** Stops clicks/drags from bubbling up to the map. */
+          Popup.prototype.stopEventPropagation = function() {
+            var anchor = this.anchor;
+            anchor.style.cursor = 'auto';
+
+            ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
+             'pointerdown']
+                .forEach(function(event) {
+                  anchor.addEventListener(event, function(e) {
+                    e.stopPropagation();
+                  });
+                });
+          };
     if(props.map !== null){
         props.locations.forEach( location => {
-            let infowindow = new google.maps.InfoWindow({
-                content: `<h3>${location.photo_title}</h3>
-                <img height = 100 px width = 80 px src = "${location.photo_file_url}"/>`
-         });
+          
+        var popup;
+
+        var contentString = document.createElement('div');
+        contentString.innerHTML = location.photo_title;
+        contentString.className = 'contentString'
+        // var close = document.createElement('BUTTON');
+        // var t = document.createTextNode("Close");
+        // close.appendChild(t); 
+        // close.addEventListener("click",popup.hide());
+        var postImg = this.img = document.createElement('img');
+        postImg.className = 'postImg'
+        postImg.src = location.photo_file_url;
+        postImg.width = 80;
+        postImg.height = 100;
+        contentString.appendChild(postImg);
+        //contentString.appendChild(close);
+
+
+        popup = new Popup(new google.maps.LatLng(location.latitude,location.longitude), contentString);//
+        //
+            // let infowindow = new google.maps.InfoWindow({
+            //     content: `<h3>${location.photo_title}</h3>
+            //     <img height = 100 px width = 80 px src = "${location.photo_file_url}"/>`
+         //});
 
          //  custom marker
         let marker = new google.maps.OverlayView();
-        let myLatlng = new google.maps.LatLng(location.latitude,location.longitude);
+        let myPos = new google.maps.LatLng(location.latitude,location.longitude);
         marker.likes = Math.ceil(Math.random()*10);  
-        marker.latlng = myLatlng;
+        marker.latlng = myPos;
         marker.map = props.map;
         marker.args = {};
         
         // custom marker  shown as html
+        marker.popup = popup;
+        popup.setMap(props.map);
+        popup.hide();
+        // folded.foldUp(); 
+        // popup.show();
 
-        marker.infowindow = infowindow;
         let like = marker.likes;
         marker.draw = function() {
 
@@ -60,7 +166,6 @@ const MarkersHandler = (props) =>{
                 div.classList.add("head");
                 div.style.position = 'absolute';
                 div.style.cursor = 'pointer';
-                //div.style.background = 'blue';
                 div.appendChild(img);
                 
                 if (typeof(self.args.marker_id) !== 'undefined') {
@@ -70,9 +175,12 @@ const MarkersHandler = (props) =>{
                 // show info click
                 google.maps.event.addDomListener(div, "click", function(event) {
                     if(preMark !== null ){
-                    preMark.infowindow.close();
+                    preMark.popup.hide();
+                    //folded.foldUp();
                     }
-                    infowindow.open(props.map, marker);
+                    //folded.fracture(-3).unfold();
+                    popup.show();
+                
                     preMark = marker;		
                     google.maps.event.trigger(self, "click");
                 });
@@ -157,3 +265,4 @@ const MarkersHandler = (props) =>{
 }
 
 export default MarkersHandler;
+
