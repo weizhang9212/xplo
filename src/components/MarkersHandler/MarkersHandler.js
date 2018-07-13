f6afcfdc6e86da1be23d09183612a31df15fe226
Xplo <xplo@Xplos-Mac-mini.local>
2018-07-12 18:33:38 -0700

    “reconstruct”

--- src/components/MarkersHandler/MarkersHandler.js
+++ src/components/MarkersHandler/MarkersHandler.js
@@ -1,159 +1,192 @@
 import React, { Component } from 'react';
 import MarkerClusterer from '@google/markerclusterer'
+import PostShow from '../PostShow/PostShow'
+import OriDomi from 'oridomi'
 
-const MarkersHandler = (props) =>{
-    const dream = [
+export default class MarkersHandler extends Component{
 
-    ];
+    state ={
+        locations:[],
+        google : null,
+        map : null,
+        postShow : false
+    }
+    cluster = null;
+    preMark = null;
+    markers = [];
 
-    // round counter 
-    let roundIndex = 0;
-    // markers we have
-    let markers = [];
-    const google = props.google;
-    let preMark = null;
-    //'mousemove click mouseup mousedown keydown keypress keyup submit change mouseenter scroll resize dblclick'
-    document.getElementsByClassName("App")[0].addEventListener('touchmove', actionMade);
-    document.getElementsByClassName("App")[0].addEventListener('click', actionMade);
-    let cluster = null;
-
-    function actionMade(){
-        if(cluster === null) return;
-        
-        cluster.actionMade();
+    componentDidMount(){
+        console.log(" markers did mount" );
+        this.loadMarkers();
     }
 
-    if(props.map !== null){
-        props.locations.forEach( location => {
-            let infowindow = new google.maps.InfoWindow({
-                content: `<h3>${location.photo_title}</h3>
-                <img height = 100 px width = 80 px src = "${location.photo_file_url}"/>`
-         });
+    componentWillUpdate(){
 
-         //  custom marker
-        let marker = new google.maps.OverlayView();
-        let myLatlng = new google.maps.LatLng(location.latitude,location.longitude);
-        marker.likes = Math.ceil(Math.random()*10);  
-        marker.latlng = myLatlng;
-        marker.map = props.map;
-        marker.args = {};
-        
-        // custom marker  shown as html
+    }
 
-        marker.infowindow = infowindow;
-        let like = marker.likes;
-        marker.draw = function() {
+    shouldComponentUpdate(nextProps){
+        console.log("shouldCOmponentUpdate");
+        //this.loadMarkers();
+        return true;
+    }
 
+    componentWillMount(){
+        console.log(" markers will mount" );
+        this.setState({locations: this.props.locations,
+            google : this.props.google,
+            map : this.props.map}
+        );
             
-            var self = this;
-            
-            var div = this.div;
-            
-            if (!div) {
-                var img = this.img = document.createElement('img');
-                div = this.div = document.createElement('div');
-                img.src = location.photo_file_url;
-                if(like > 5) {
-                    img.classList.add("like");
-                }
-                div.className = 'marker';
-                div.classList.add("head");
-                div.style.position = 'absolute';
-                div.style.cursor = 'pointer';
-                //div.style.background = 'blue';
-                div.appendChild(img);
-                
-                if (typeof(self.args.marker_id) !== 'undefined') {
-                    div.dataset.marker_id = self.args.marker_id;
-                }
-                
-                // show info click
-                google.maps.event.addDomListener(div, "click", function(event) {
-                    if(preMark !== null ){
-                    preMark.infowindow.close();
-                    }
-                    infowindow.open(props.map, marker);
-                    preMark = marker;		
-                    google.maps.event.trigger(self, "click");
-                });
+    }
 
-                // long press function
+    loadMarkers(){
+        document.getElementsByClassName("App")[0].addEventListener('touchmove', this.actionMade);
+        document.getElementsByClassName("App")[0].addEventListener('click', this.actionMade);
+        const that = this;
 
-                // start to press marker
-                let pressTimer;
-                google.maps.event.addDomListener(div, "touchstart", function(event){
-                    pressTimer = setTimeout(()=>{
-                        if(cluster){
-                            cluster.clearTimer();
+        if(this.state.map != null){
+            const google = this.props.google;
+            this.state.locations.forEach( location => {
+                let infowindow = new google.maps.InfoWindow({
+                    content: `<div class = "post"><h3 >${location.photo_title}</h3>
+                    <img height = 100 px width = 80 px src = "${location.photo_file_url}"/>
+                    </div>`
+                })
+                // custom marker
+                let marker = new google.maps.OverlayView();
+                let myLatlng = new google.maps.LatLng(location.latitude,location.longitude);
+                marker.likes = Math.ceil(Math.random()*10);  
+                marker.latlng = myLatlng;
+                marker.map = this.state.map;
+                marker.args = {};
+
+                //custom marker shown as html
+
+                marker.infowindow = infowindow;
+
+                marker.draw = function() {
+                    var self = this;              
+                    var div = this.div;
+                    let like = marker.likes;
+                    if (!div) {
+                        var img = this.img = document.createElement('img');
+                        div = this.div = document.createElement('div');
+                        img.src = location.photo_file_url;
+                        if(like > 5) {
+                            img.classList.add("like");
                         }
-                        props.map.getBounds().extend();
-                    },2000);
-                });
-                
-                // press end
-                google.maps.event.addDomListener(div, "touchend", function(event){
-                    clearTimeout(pressTimer);
-                    if(cluster){
-                        cluster.activeTimer();
+                        div.className = 'marker';
+                        div.classList.add("head");
+                        div.style.position = 'absolute';
+                        div.style.cursor = 'pointer';
+                        //div.style.background = 'blue';
+                        div.appendChild(img);
+                        
+                        if (typeof(self.args.marker_id) !== 'undefined') {
+                            div.dataset.marker_id = self.args.marker_id;
+                        }                
+                        // show info click
+                        google.maps.event.addDomListener(div, "click", function(event) {
+                            if(that.preMark !== null ){
+                            that.preMark.infowindow.close();
+                            }
+
+                            //
+                            that.cluster.clearTimer();
+                            let show = that.state.postShow;
+                            that.setState({postShow : !show});
+                            //
+
+                            infowindow.open(that.state.map, marker);
+                            that.preMark = marker;		
+                            google.maps.event.trigger(self, "click");
+                            
+                            // document.getElementById('test').addEventListener('click', function() {
+                            //     oriDomi.collapse(-40);
+                            // });
+                        });
+                        // long press function
+        
+                        // start to press marker
+                        let pressTimer;
+                        google.maps.event.addDomListener(div, "touchstart", function(event){
+                            pressTimer = setTimeout(()=>{
+                                if(that.cluster){
+                                    that.cluster.clearTimer();
+                                }
+                                that.state.map.getBounds().extend();
+                            },2000);
+                        });
+                        
+                        // press end
+                        google.maps.event.addDomListener(div, "touchend", function(event){
+                            clearTimeout(pressTimer);
+                            if(that.cluster){
+                                that.cluster.activeTimer();
+                            }
+                            console.log(that.cluster);
+                        });
+                        
+                        var panes = this.getPanes();
+                        panes.overlayImage.appendChild(div);
+                    }      
+                    // set the position of div
+                    var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+                    
+                    if (point) {
+                        div.style.left = (point.x - 10) + 'px';
+                        div.style.top = (point.y - 20) + 'px';
                     }
-                    console.log(cluster);
-                });
+                    marker.div = div;
+                }
+
+                marker.remove = function() {
+                    if (this.div) {
+                        this.div.parentNode.removeChild(this.div);
+                        this.div = null;
+                    }	
+                };
                 
-                var panes = this.getPanes();
-                panes.overlayImage.appendChild(div);
-            }
-            
-            // set the position of div
-            var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-            
-            if (point) {
-                div.style.left = (point.x - 10) + 'px';
-                div.style.top = (point.y - 20) + 'px';
-            }
-            marker.div = div;
-        };
-        
-        marker.remove = function() {
-            if (this.div) {
-                this.div.parentNode.removeChild(this.div);
-                this.div = null;
-            }	
-        };
-        
-        marker.getPosition = function() {
-            return this.latlng;	
-        };
-        marker.setMap(props.map);
+                marker.getPosition = function() {
+                    return this.latlng;	
+                };
+                marker.setMap(that.state.map);
+                /*    end of custom marker        */
 
-        /*    end of custom marker        */
+                that.markers.push(marker);
+            })
 
-
-
-        markers.push(marker);
-        })
-
-
-        let markerClusterer = new MarkerClusterer(props.map, markers, {
-            maxZoom: 15,
-            gridSize: 20,
-            style:10,
-            optimized:false,
-            imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-          });
-          cluster = markerClusterer;
-
-        var myoverlay = new google.maps.OverlayView();
-        myoverlay.draw = function () {
-            this.getPanes().markerLayer.id='markerLayer';
-        };
-        myoverlay.setMap(props.map);
-
-
-        
+            let markerClusterer = new MarkerClusterer(this.state.map, this.markers, {
+                maxZoom: 15,
+                gridSize: 20,
+                style:10,
+                optimized:false,
+                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+              });
+            this.cluster = markerClusterer;
+        }
     }
-    return (
-        <div></div>
-    )
-}
+    actionMade=()=>{
+        if(this.cluster === null) return;
+        this.cluster.actionMade();
+    }
 
-export default MarkersHandler;
+
+    
+
+
+    
+    render(){
+        
+        console.log("render");
+        return(
+        <div>{this.state.postShow? 
+          <PostShow google={this.props.google}
+                    map = {this.props.map}
+                    />  : 
+            null}
+        <button id = "test" >oridomi test</button>    
+        </div>
+    );
+    }
+}
